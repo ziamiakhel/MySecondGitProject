9662729924c2012919c5420e13efebf5146caae7
ziamiakhel <zmiakhel@gmail.com>
2018-03-10 11:44:43 -0500

    My initial commit

---
+++ src/main/GoodEvening.java
@@ -0,0 +1,8 @@
+package main;
+
+public class GoodEvening {
+
+	public static void main(String[] args) {
+		System.out.println("Good evening Class!");
+	}
+}
---
+++ src/main/GoodMorning.java
@@ -0,0 +1,8 @@
+package main;
+
+public class GoodMorning {
+
+	public static void main(String[] args) {
+		System.out.println("Good morning Class!");
+	}
+}
